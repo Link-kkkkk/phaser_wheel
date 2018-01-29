75ef79bbcc9eba3ecf6a66978c33009364a96aa1
Link <13788485679@sina.cn>
2018-01-29 11:36:48 +0800

    默认路由指向连连看demo

--- linkup/src/router/index.js
+++ linkup/src/router/index.js
@@ -13,12 +13,12 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: '/actweb/linkup/',
-  // base: '/',
+  // base: '/actweb/linkup/',
+  base: '/',
   routes: [
     {
-      path: '/index',
-      component: index
+      path: '/',
+      component: linkupGame,
     },
     {
       path: '/linkup',
